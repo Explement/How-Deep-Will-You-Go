8bd1217b8fad9557dfbb3d3474456e177f276705
yuval-sys <davidov.yuval@gmail.com>
2024-11-07 23:47:24 +0200

    added sword cooldown

--- how-deep-will-you-go/how-deep-will-you-go/scripts/player-movement.gd
+++ how-deep-will-you-go/how-deep-will-you-go/scripts/player-movement.gd
@@ -8,7 +8,7 @@ var speed = 250
 var health = 10
 var attack_damage = 5
 var enemy_in_range = false
-
+var cooldown = false
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
@@ -20,9 +20,11 @@ func _input(event):
 		strike()
 	
 func strike():
-	if enemy_in_range == true:
+	if enemy_in_range == true and cooldown == false:
 		emit_signal("attack", attack_damage)
 		%AnimationPlayer.play("attack")
+		cooldown = true
+		%sword_cooldown.start()
 	else:
 		%AnimationPlayer.play("attack")
 		
@@ -32,10 +34,11 @@ func strike():
 func _on_static_body_2d_body_entered(body):
 	if (body.name == "CharacterBody2D"):
 		enemy_in_range = true
-		
-	
-		
-
-
 func _on_static_body_2d_body_exited(body):
-	enemy_in_range = false
+	if (body.name == "CharacterBody2D"):
+		enemy_in_range = false
+
+
+func _on_sword_cooldown_timeout():
+	cooldown = false
+	%sword_cooldown.stop()
