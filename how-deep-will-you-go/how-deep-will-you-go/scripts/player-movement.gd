3509a123e1af8eb66d883cf376242c61d60cb7ef
yuval-sys <davidov.yuval@gmail.com>
2024-11-08 19:11:23 +0200

    added flip_h to player

--- how-deep-will-you-go/how-deep-will-you-go/scripts/player-movement.gd
+++ how-deep-will-you-go/how-deep-will-you-go/scripts/player-movement.gd
@@ -9,36 +9,63 @@ var health = 10
 var attack_damage = 5
 var enemy_in_range = false
 var cooldown = false
+var is_attacking = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("strike"):
+		strike()
+	if velocity.x <= 0 and is_attacking == false:
+		%animations.play("sword_left")
+	elif velocity.x >= 0 and is_attacking == false:
+		%animations.play("sword_right")
+		
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
 	move_and_slide()
-
+	if velocity.x <= 0:
+		%player_sprite.flip_h = true
+		%sword_sprite.flip_h = true
+		%sword_collision_front.disabled = true
+		%sword_collision_back.disabled = false
+	elif velocity.x >= 0:
+		%player_sprite.flip_h = false
+		%sword_sprite.flip_h = false
+		%sword_collision_back.disabled = true
+		%sword_collision_front.disabled = false
+		
 # combat
-func _input(event):
-	if event.is_action_pressed("strike"):
-		strike()
-	
 func strike():
+	is_attacking = true
+	%attack_animation_overrule_timer.start()
+	if velocity.x > 0:
+		%animations.play("attack_front")
+	elif velocity.x < 0:
+		%animations.play("attack_back")
+	elif velocity.x == 0:
+		%animations.play("attack_back")
+		print("goog")
 	if enemy_in_range == true and cooldown == false:
 		emit_signal("attack", attack_damage)
-		%AnimationPlayer.play("attack")
 		cooldown = true
 		%sword_cooldown.start()
-	else:
-		%AnimationPlayer.play("attack")
+		
 		
 	
 
 
 func _on_static_body_2d_body_entered(body):
-	if (body.name == "CharacterBody2D"):
+	if (body.name == "player"):
 		enemy_in_range = true
 func _on_static_body_2d_body_exited(body):
-	if (body.name == "CharacterBody2D"):
+	if (body.name == "player"):
 		enemy_in_range = false
-
 
 func _on_sword_cooldown_timeout():
 	cooldown = false
 	%sword_cooldown.stop()
+
+
+func _on_attack_animation_overrule_timer_timeout():
+	is_attacking = false
+	%attack_animation_overrule_timer.stop()
